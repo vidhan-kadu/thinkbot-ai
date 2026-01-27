1a9db40655c9ec31610912c54b20d1ec0c61c13d
vidhan <vidhankadu@gmail.com>
2026-01-28 03:17:12 +0530

    Add typing effect for assistant responses

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -1,36 +1,56 @@
 import "./Chat.css";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { MyContext } from "./MyContext";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import "highlight.js/styles/github-dark.css";
 
-
-
 function Chat() {
-  const { newChat, prevChats } = useContext(MyContext);
+  const { newChat, prevChats, reply } = useContext(MyContext);
+  const [ latestReply, setLatestReply ] = useState(null);
+
+  useEffect(() => {
+    //latestReply separate => typing effect create
+    if (!prevChats?.length) return;
+
+    const content = reply.split(" "); //individual words
+
+    let idx = 0;
+    const interval = setInterval(() => {
+      setLatestReply(content.slice(0, idx+1).join(" "));
+      idx++;
+      if (idx >= content.length) clearInterval(interval);
+    }, 40);
+
+    return () => clearInterval(interval);
+  }, [prevChats, reply]);
 
   return (
     <>
       {newChat && <h1>Start a New Chat!</h1>}
       <div className="chats">
-        
-        {
-        prevChats?.map((chat, idx) => 
+        {prevChats?.slice(0, -1).map((chat, idx) => 
           <div
-            className={chat.role === "user"? "userDiv" : "gptDiv"}
+            className={chat.role === "user" ? "userDiv" : "gptDiv"}
             key={idx}
           >
-            {
-            chat.role === "user"? 
+            {chat.role === "user" ? 
               <p className="userMessage">{chat.content}</p>
              : 
-             <ReactMarkdown rehypePlugins={rehypeHighlight}>{chat.content}</ReactMarkdown>
+              <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
+                {chat.content}
+              </ReactMarkdown>
             }
           </div>
-        )
-        }
+        )}
 
+        {prevChats.length > 0 && latestReply !== null && 
+          <div className="gptDiv" key={"typing"}>
+            <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
+              {latestReply}
+            </ReactMarkdown>
+          </div>
+        }
       </div>
     </>
   );
